b708737c20f9a16160113e3089455c24978fefc1
Ryan Richard <klaethecoder@gmail.com>
2018-10-04 21:16:33 -0600

    Updated the code with comments and explainations of what is going on within the document.

--- boxes/boxes.js
+++ boxes/boxes.js
@@ -1,3 +1,4 @@
+// Array of assorted colors
 const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
     '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
     '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
@@ -10,12 +11,15 @@ const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
     '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
 ];
 
+//Selections of the main div, h1, and links on the page for DOM manipulation. 
 const div = document.querySelector(".boxes")
 const h1 = document.querySelector("h1")
 const links = document.querySelectorAll('a')
 
+// Function that creates a set number of boxes displayed on the page
 function createBoxes(number) {
     let colorIndex = 0;
+    // Loops through a set number of times, creates new boxes, assigns color from the array, and pushes it to the main div.
     for (let i = 0; i < number; i++) {
         let box = document.createElement('div');
         box.style = `background-color: ${colors[colorIndex]} `
@@ -26,6 +30,8 @@ function createBoxes(number) {
             colorIndex = 0;
         } else { colorIndex++ }
     }
+
+    //Puts an Event Listener on the main div that removes the target (child) element and updates the amount of boxes on the page.
     div.addEventListener('click', function(e) {
         e.target.remove();
         h1.textContent = `There are ${div.querySelectorAll('.box').length} boxes on the page`;
@@ -33,6 +39,8 @@ function createBoxes(number) {
 }
 
 for (let i = 0; i < links.length; i++) {
+    /* Loops through all of the links on the page and adds an Event Listener on them to put a set amount of boxes on the page and 
+     update the amount of boxes on the page */
     links[i].addEventListener('click', function() {
         div.innerHTML = ''
         createBoxes(Number(links[i].textContent))
